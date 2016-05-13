54280cb630d2ad430f9b14bb224068660338a8e2
ratclifflab <ratcliff@gatech.edu>
2016-05-13 02:48:50 -0400

    Update ND2 to JPG.py

--- ND2 to JPG.py
+++ ND2 to JPG.py
@@ -13,12 +13,12 @@ import os
 def run():
     gd = GenericDialogPlus("ND2 Conversion Tool")
     gd.addMessage("This plugin uses BioFormats to convert ND2 images to various file formats for further processing in ImageJ.")
-    gd.addDirectoryOrFileField("Input: ", "Z:\\") #srcDir
-    gd.addDirectoryOrFileField("Output: ", "Z:\\") #dstDir
+    gd.addDirectoryOrFileField("Input: ", "G:\\Subdir testing\\") #srcDir
+    gd.addDirectoryOrFileField("Output: ", "G:\\Subdir testing\\") #dstDir
     gd.addStringField("File name contains: ", "") #nameContains
     gd.addCheckbox("Preserve directory structure?", True) #keepDirs
-    gd.addCheckbox("Run in headless mode?", False) #anneBoleyn 
-    #overwrite existing JPGs?
+    gd.addCheckbox("Run in headless mode?", True) #anneBoleyn 
+    gd.addCheckbox("Overwrite existing output files?", True)
     gd.showDialog()
     if gd.wasCanceled():
         return
@@ -27,6 +27,8 @@ def run():
     nameContains = gd.getNextString()
     keepDirectories = gd.getNextBoolean()
     anneBoleyn = gd.getNextBoolean()
+    overOut = gd.getNextBoolean()
+    IJ.run("Input/Output...", "jpeg=100")
     for root, directories, filenames in os.walk(srcDir):
         for filename in filenames:
             # Check for file extension
@@ -35,24 +37,29 @@ def run():
             # Check for file name pattern
             if nameContains not in filename:
                 continue
-            process(srcDir, dstDir, nameContains, root, filename, keepDirectories, anneBoleyn)
+            process(srcDir, dstDir, nameContains, root, filename, keepDirectories, anneBoleyn, overOut)
 
-def process(srcDir, dstDir, nameContains, currentDir, fileName, keepDirectories, anneBoleyn):
+def process(srcDir, dstDir, nameContains, currentDir, fileName, keepDirectories, anneBoleyn, overOut):
     print "Opening", fileName
     saveDir = currentDir.replace(srcDir, dstDir) if keepDirectories else dstDir
+    head, tail = os.path.splitext(fileName)
     imps = BF.openImagePlus(os.path.join(currentDir, fileName))
     print "Opened", fileName
     for imp in imps:
         if not anneBoleyn:
             print "Now showing image..."
             imp.show()
+        IJ.run(imp, "Blue", "")
     fs = FileSaver(imp)
     if not os.path.exists(saveDir):
         os.makedirs(saveDir)
-    print "Saving to", saveDir
-    fs.saveAsJpeg(os.path.join(saveDir, fileName))
-    #IJ.selectWindow("Log")
-    #IJ.run("Close")
+    if not overOut:
+        if os.path.exists(saveDir + "\\" + head + ".jpg"):
+            print "This JPG exists already and will not be overwritten. Moving on..."
+    else:
+        print "Saving to", os.path.join(saveDir, head + ".jpg")
+        fs.saveAsJpeg(os.path.join(saveDir, head + ".jpg"))
     imp.close()
 
 run()
+print "Operation complete!"
